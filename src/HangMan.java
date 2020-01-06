7558005cee4c1e299492d2507984d1f3b7b400ad
Sean Montague <stmontague1@gmail.com>
2020-01-05 22:19:02 -0600

    Finished key game functionality

--- src/HangMan.java
+++ src/HangMan.java
@@ -4,14 +4,14 @@ import  java.util.Scanner;
 
 class HangMan {
     public static void main(String[] args) {
-        boolean gameStatus, discoveredWord = false;
-        int remainingGuesses = 5, neededLetters;
+        boolean gameStatus, discoveredWord;
+        int remainingGuesses, neededLetters, letCnt;
         String guess, goal;
+        StringBuilder attempt;
         char charGuess;
         String[] wordList = {"pickles", "carnival", "dance", "karaoke"};
         Random rand = new Random();
-        goal = wordList[rand.nextInt(wordList.length)];
-        neededLetters = goal.length();
+
         HashSet<Character> incorrectGuesses = new HashSet<>();
         HashSet<Character> correctGuesses = new HashSet<>();
 
@@ -25,6 +25,17 @@ class HangMan {
         while (gameStatus) {
             System.out.println("We have started our Hangman game! ");
 
+            // Initialize game state
+            goal = wordList[rand.nextInt(wordList.length)];
+            neededLetters = goal.length();
+            remainingGuesses = 5;
+            correctGuesses.clear();
+            incorrectGuesses.clear();
+            discoveredWord = false;
+            attempt = new StringBuilder();
+            initializeAttempt(attempt, neededLetters);
+
+
             while (remainingGuesses > 0 && !discoveredWord) {
                 System.out.println("You have " + remainingGuesses +
                         " incorrect guesses remaining.");
@@ -34,41 +45,60 @@ class HangMan {
                     !incorrectGuesses.contains(guess.charAt(0))) {
                     charGuess = guess.charAt(0);
                     if (compareGuessToGoal(charGuess, goal)) {
-                        neededLetters--;
+                        letCnt = getNumMatches(charGuess, goal, attempt);
+                        neededLetters -= letCnt;
                         correctGuesses.add(charGuess);
                         System.out.println("Hooray, " + guess +
-                                " is in the mystery word!");
+                                " is in the mystery word! The current word board is " + attempt);
                     } else {
                         remainingGuesses--;
                         incorrectGuesses.add(charGuess);
                         System.out.println("Ugh, " + guess +
                                 " is not in the mystery word! You have " +
-                                remainingGuesses + "left!");
+                                remainingGuesses + " guesses left! The current word board is " + attempt);
                     }
                     if (neededLetters == 0) {
                         discoveredWord = true;
                     }
                 } else {
-                    System.out.println("Invalid guess, only letters will be accepted!");
+                    System.out.println("Invalid guess, only single, not previously guessed letters will be accepted!");
                 }
             }  // end while
             if (discoveredWord) {
                 System.out.println("Congrats, you won!");
             } else {
-                System.out.println("Nice try, but you lost!");
+                System.out.println("Nice try, but you lost! The hidden word was " + goal);
             }
+
             gameStatus = wantsToPlayHangman();
         }  // end while
         System.out.println("Thanks for playing Hangman");
     }
 
+    private static void initializeAttempt(StringBuilder attempt, int neededLetters) {
+        for (int i = 0; i < neededLetters; i++) {
+            attempt.append("_");
+        }
+    }
+
+    private static int getNumMatches(char charGuess, String goal, StringBuilder attempt) {
+        int cnt = 0;
+        for (int i = 0; i < goal.length(); i++) {
+            if (goal.charAt(i) == charGuess) {
+                cnt++;
+                attempt.replace(i, i + 1, charGuess + "");
+            }
+        }
+        return cnt;
+    }
+
     private static boolean wantsToPlayHangman() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Do you want to play Hangman");
+        System.out.println("Do you want to play Hangman? (yes/no)");
         String playGame = input.nextLine().toLowerCase();
         while (!"yes".equals(playGame) && !"no".equals(playGame)) {
             System.out.println("Sorry, I don't understand. Do you want " +
-                    " to play Hangman?");
+                    " to play Hangman? (yes/no)");
             playGame = input.nextLine();
         }
         return "yes".equals(playGame);
